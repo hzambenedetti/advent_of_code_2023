1ff886a32221032e558370cb1918a4babb0ca8f9
HenriqueZ18 <Hique.rz03@gmail.com>
2024-01-08 11:43:06 -0300

    Implemented brute force solution for part two of day five

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use solution::solution_one;
+use solution::{solution_one, solution_two};
 
 mod solution;
 mod parser;
@@ -8,8 +8,10 @@ mod parser;
 fn main() {
     let input = fs::read_to_string("input/day_05.txt").unwrap();
     let (seeds, maps) = parser::parse_input(&input);
-    
+
     let solution_one = solution_one(&seeds, &maps);
+    let solution_two = solution_two(&seeds, &maps);
 
     println!("answer one: {solution_one}");
+    println!("answer two: {solution_two}");
 }
--- day_05/src/solution.rs
+++ day_05/src/solution.rs
@@ -18,3 +18,24 @@ pub fn solution_one(seeds: &Vec<usize>, maps: &Vec<Map>) -> usize{
 
     min
 }
+
+pub fn solution_two(seeds: &Vec<usize>, maps: &Vec<Map>) -> usize{
+    let mut min: usize = usize::MAX;
+    
+    for x in (0..seeds.len()).step_by(2){
+        for seed in seeds[x]..(seeds[x] + seeds[x + 1]){
+            let mut mapped_seed = seed;
+            for map in maps.iter(){
+                for &(src, dest, len) in map.mapping.iter(){
+                    if mapped_seed >= src && mapped_seed <= src + len - 1{
+                        mapped_seed = dest + (mapped_seed - src);
+                        break;
+                    }
+                }
+            }
+            min = min.min(mapped_seed);
+        }
+    }
+
+    min
+}
